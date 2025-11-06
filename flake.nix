15c0a78b86d4ddb517c0ed5d46be1f192cfdac29
Jonas Bork <jonas10bork@gmail.com>
2025-11-06 14:41:38 +0100

    Add Nix for installing Ansible and locking the version

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              ansible
+            ];
+          };
+        };
+    };
+}
